91c3183bc9d862512e9be11349ea17246facb56a
Thomas Ismaïl Murzyn <thomasismailmurzyn@mbp-de-thomas.lan>
2021-11-10 13:25:32 +0100

    first commit :  Initializing the project with my routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Welcome to Vinted</div>;
+};
+
+export default Home;
