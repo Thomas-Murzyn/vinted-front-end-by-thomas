2bfc75e4ccac43f1526cb84332ae951782d548e9
Thomas Ismaïl Murzyn <thomasismailmurzyn@mbp-de-thomas.lan>
2021-11-10 14:26:15 +0100

    adding an axios request. Getting the data from the backend api and display offers on the home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,43 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  return (
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://vinted-api-le-reacteur.herokuapp.com/offers"
+        );
+
+        setData(response.data);
+        setIsLoading(true);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return !isLoading ? (
+    <div>...Downloading</div>
+  ) : (
     <div>
       <h1>Welcome to Vinted</h1>
-      <Link to="/offer">Allez sur la page Offer</Link>
+      {data.allOffers.map((offer, index) => {
+        return (
+          <div>
+            <p>{offer.product_name}</p>
+            <p>{offer.product_price}</p>
+            <img className="offer-image" src={offer.product_image.url} alt="" />
+          </div>
+        );
+      })}
+      {/* <Link to="/offer">Allez sur la page Offer</Link> */}
     </div>
   );
 };
