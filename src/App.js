37fcb9ba3d0498e18b3bcb26d26c2124cc81d147
Thomas Ismaïl Murzyn <thomasismailmurzyn@mbp-de-thomas.lan>
2021-11-10 13:40:31 +0100

    adding header and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../assets/Vinted-logo.svg.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="" />
+    </header>
+  );
+};
+
+export default Header;
