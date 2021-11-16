676adc619d128b7519aabb350bfc9c44332ee330
Thomas Ismaïl Murzyn <thomasismailmurzyn@mbp-de-thomas.lan>
2021-11-16 15:08:02 +0100

    test

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,3 +1,7 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../components/CheckoutForm";
+
 const Payment = () => {
   return <div>Payment</div>;
 };
